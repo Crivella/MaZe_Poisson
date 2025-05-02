4ed77b36ac9ad68818546d53097023952718d9ba
crivella <davidecrivella@yahoo.it>
2025-05-02 12:36:37 +0200

    Fix missing

--- library/src/charges.c
+++ library/src/charges.c
@@ -40,7 +40,8 @@ double spread_spline_cubic(double x, double L, double h) {
 
 double update_charges(
     int n_grid, int n_p, double h,
-    double *pos, long int *neighbors, long int *charges, double *q
+    double *pos, long int *neighbors, long int *charges, double *q,
+    double (*g)(double, double, double)
 ) {
     int n_loc = get_n_loc();
     int n_loc_start = get_n_start();
@@ -91,65 +92,13 @@ double update_charges(
     return q_tot;
 }
 
-double update_charges_splquadr(
-    int n_grid, int n_p, double h,
-    double *pos, long int *neighbors, long int *charges, double *q
-) {
-    int n_loc = get_n_loc();
-    int n_loc_start = get_n_start();
-
-    long int ni, nj, nk, ni_loc;
-    long int i1, i2;
-    long int n2 = n_grid * n_grid;
-    long int n3 = n_loc * n2;
-
-    double px, py, pz;
-
-    double L = n_grid * h;
-    double q_tot = 0.0;
-    long int chg;
-    double app, upd;
-
-    #pragma omp parallel for
-    for (long int i=0; i < n3; i++) {
-        q[i] = 0.0;
-    }
-
-    #pragma omp parallel for private(i1, i2, ni, nj, nk, ni_loc, px, py, pz, app, upd, chg) reduction(+:q_tot)
-    for (long int i=0; i<n_p; i++) {
-        i1 = i * 3;
-        i2 = i * 64 * 3;
-
-        chg = charges[i];
-        px = pos[i1 + 0];
-        py = pos[i1 + 1];
-        pz = pos[i1 + 2];
-        for (int j=0; j < 64; j+=3) {
-            ni = neighbors[i2 + j + 0];
-            ni_loc = ni - n_loc_start;
-            if (ni_loc < 0 || ni_loc >= n_loc) {
-                continue;
-            }
-            nj = neighbors[i2 + j + 1];
-            nk = neighbors[i2 + j + 2];
-            app = g(px - ni*h, L, h) * g(py - nj*h, L, h) * g(pz - nk*h, L, h);
-            upd = chg * app;
-            q_tot += upd;
-            q[ni_loc * n2 + nj * n_grid + nk] += upd;
-        }
-    }
-
-    allreduce_sum(&q_tot, 1);
-
-    return q_tot;
-}
-
 #else
 
 
 double update_charges(
     int n_grid, int n_p, double h,
-    double *pos, long int *neighbors, long int *charges, double *q
+    double *pos, long int *neighbors, long int *charges, double *q,
+    double (*g)(double, double, double)
 ) {
     long int ni, nj, nk;
     long int i, i1, i2;
