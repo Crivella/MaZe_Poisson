Fixed charge computation for F_FD and SetCharges when overlap is present

maze_poisson/grid.py
@@ -233,46 +233,53 @@ class Grid:
         self.potential_notelec = pe
     '''
 
-    # update charges with a weight function that spreads it on the grid
-    def SetCharges(self):
+    def SetCharges(self, uniform=False):
         L = self.L
         h = self.h
         self.q = np.zeros(self.shape, dtype=float)
-        
-        # for m in range(len(self.particles.charges)):
-        #     for i, j, k in self.particles.neighbors[m, :, :]:
-        #         diff = self.particles.pos[m] - np.array([i,j, k]) * h
-        #         self.q[i, j, k] += self.particles.charges[m] * g(diff[0], L, h) * g(diff[1], L, h) * g(diff[2], L, h)
 
-        # Same as above using broadcasting
-        diff = self.particles.pos[:, np.newaxis, :] - self.particles.neighbors * h
-        
-        # if Python 3.11 or newer uncomment below and comment lines 217-219
-        #self.q[*self.particles.neighbors.reshape(-1, 3).T] += (self.particles.charges[:, np.newaxis] * np.prod(g(diff, L, h), axis=2)).flatten()
-        
-        # Version that works for Python 3.8.15
-        indices = tuple(self.particles.neighbors.reshape(-1, 3).T)
-        
+        # Spargi le prime due particelle (0 e 1) normalmente
+        indices_normal = [0, 1]
+        pos_normal = self.particles.pos[indices_normal]
+        neighbors_normal = self.particles.neighbors[indices_normal]  # shape: (2, 8, 3)
+        charges_normal = self.particles.charges[indices_normal]      # shape: (2,)
+
+        # Calcola le differenze r_i - r_alpha
+        diff = pos_normal[:, np.newaxis, :] - neighbors_normal * h  # shape: (2, 8, 3)
+
+        # Appiattisci gli indici (shape: (3, 16)) per usarli su griglia
+        indices = tuple(neighbors_normal.reshape(-1, 3).T)
+
+        # Calcola i pesi g in base al tipo di interpolazione
         if self.grid_setting.cas == 'CIC':
-            updates = (self.particles.charges[:, np.newaxis] * np.prod(g(diff, L, h), axis=2)).flatten()
+            weights = np.prod(g(diff, L, h), axis=2)  # shape: (2, 8)
         elif self.grid_setting.cas == 'B-Spline':
-            updates = (self.particles.charges[:, np.newaxis] * np.prod(cubic_bspline(diff, L, h), axis=2)).flatten()
+            weights = np.prod(cubic_bspline(diff, L, h), axis=2)
         elif self.grid_setting.cas == 'Quadratic-B-Spline':
-            # w = quadratic_bspline(diff[0], L, h)
-            # print("Weights:", np.sum(np.prod(w, axis=1)))
-            updates = (self.particles.charges[:, np.newaxis] * np.prod(quadratic_bspline(diff, L, h), axis=2)).flatten()
+            weights = np.prod(quadratic_bspline(diff, L, h), axis=2)
         else:
             raise ValueError(f"Unknown grid setting: {self.grid_setting.cas}")
-        
-        self.q[indices] += updates
-  
-        q_tot_expected = np.sum(self.particles.charges)
-        q_tot = np.sum(self.q)
 
-        if q_tot + 1e-6 < q_tot_expected:
-            logger.error('Error: change initial position, charge is not preserved: q_tot ='+str(q_tot))
-            exit() # exits running otherwise it hangs the code
-                
+        updates = (charges_normal[:, np.newaxis] * weights).flatten()  # shape: (16,)
+
+        # Aggiorna la griglia in modo sicuro anche in presenza di indici duplicati
+        np.add.at(self.q, indices, updates)
+
+        # Se richiesto, distribuisci uniformemente la terza carica
+        if uniform:
+            q_uniform = self.particles.charges[2]
+            V = np.prod(self.q.shape)
+            self.q += q_uniform / V
+
+        # Verifica la conservazione della carica
+        q_tot_expected = np.sum(self.particles.charges)
+        q_tot_actual = np.sum(self.q)
+
+        if q_tot_actual + 1e-6 < q_tot_expected:
+            logger.error('Error: change initial position, charge is not preserved: q_tot = ' + str(q_tot_actual))
+            exit()
+    
+    
     # returns only kinetic energy and not electrostatic one
     def Energy(self, iter, print_energy, prev=False):
         # kinetic E
@@ -422,6 +429,8 @@ class Grid:
         # # plt.plot(np.arange(self.N) * self.h * a0, line_p, label='H_p', marker='.', linestyle='-')
         # plt.xlabel('x [Å]')
         # plt.axvline(pos[0,0] * a0, label='$x_1$', color='k', linestyle='-')
+        # print(radius[0] * a0, radius[1] * a0, w * a0)
+        # print()
         
         # plt.axvline(a0 * (pos[0,0] + radius[0] - w), color='r', label='$R_1$ - w')
         # plt.axvline(a0 * (pos[0,0] + radius[0] + w), color='orange', label='$R_1$ + w')

maze_poisson/particles.py
@@ -70,11 +70,12 @@ class Particles:
             self.neighbors = np.empty((self.N_p, 64, 3), dtype=int)
       
         # Compute pairwise TF parameters
-        charges_sum = self.charges[:, np.newaxis] + self.charges[np.newaxis, :]  # Shape: (N_p, N_p)
-        A = self.A = np.vectorize(lambda q: self.tf_params[q][0])(charges_sum)
-        C = self.C = np.vectorize(lambda q: self.tf_params[q][1])(charges_sum)
-        D = self.D = np.vectorize(lambda q: self.tf_params[q][2])(charges_sum)
-        sigma_TF = self.sigma_TF = np.vectorize(lambda q: self.tf_params[q][3])(charges_sum)
+        if md_variables.not_elec:
+            charges_sum = self.charges[:, np.newaxis] + self.charges[np.newaxis, :]  # Shape: (N_p, N_p)
+            A = self.A = np.vectorize(lambda q: self.tf_params[q][0])(charges_sum)
+            C = self.C = np.vectorize(lambda q: self.tf_params[q][1])(charges_sum)
+            D = self.D = np.vectorize(lambda q: self.tf_params[q][2])(charges_sum)
+            sigma_TF = self.sigma_TF = np.vectorize(lambda q: self.tf_params[q][3])(charges_sum)
 
             V_shift = A * np.exp(self.B * (sigma_TF - self.r_cutoff)) - C / self.r_cutoff**6 - D / self.r_cutoff**8
             alpha = self.alpha = A * self.B * np.exp(self.B * (sigma_TF - self.r_cutoff)) - 6 * C / self.r_cutoff**7 - 8 * D / self.r_cutoff**9
@@ -348,18 +349,33 @@ class Particles:
             E_x = (np.roll(delta_phi, -1, axis=0) - np.roll(delta_phi, 1, axis=0)) / (2 * h)
             E_y = (np.roll(delta_phi, -1, axis=1) - np.roll(delta_phi, 1, axis=1)) / (2 * h)
             E_z = (np.roll(delta_phi, -1, axis=2) - np.roll(delta_phi, 1, axis=2)) / (2 * h)
-
+            
             neighbors = self.neighbors  # shape: (n_particles, 8, 3)
-            q_neighbors = self.grid.q[neighbors[:, :, 0], neighbors[:, :, 1], neighbors[:, :, 2]]  # (n_particles, 8)
+            neigh_coords = neighbors * h  # (N_p, 8, 3)
 
+            # Compute r_alpha - r_i for all neighbors
+            # Shape: (N_p, 8, 3)
+            diffs = self.pos[:, None, :] - neigh_coords  # shape: (N_p, 8, 3)
+
+            # Calcola g e g_prime per ciascuna componente (shape: N_p, 8)
+            gx  = g(diffs[..., 0], L, h)
+            gy  = g(diffs[..., 1], L, h)
+            gz  = g(diffs[..., 2], L, h)
+
+            # Calcola q_neighbors come prodotto della carica con i g specifici
+            q_neighbors = self.charges[:, None] * gx * gy * gz  # shape: (N_p, 8)
+            # q_neighbors = self.grid.q[neighbors[:, :, 0], neighbors[:, :, 1], neighbors[:, :, 2]]  # (N_p, 8)
+
+            # Estrai i campi elettrici nei vicini
             E_neighbors = np.stack([
                 E_x[neighbors[:, :, 0], neighbors[:, :, 1], neighbors[:, :, 2]],
                 E_y[neighbors[:, :, 0], neighbors[:, :, 1], neighbors[:, :, 2]],
                 E_z[neighbors[:, :, 0], neighbors[:, :, 1], neighbors[:, :, 2]],
-            ], axis=-1)  # shape: (n_particles, 8, 3)
+            ], axis=-1)  # shape: (N_p, 8, 3)
 
-            self.forces_rf = -np.sum(q_neighbors[:, :, np.newaxis] * E_neighbors, axis=1)  # (n_particles, 3)
-            
+            # Calcola le forze
+            self.forces_rf = -np.sum(q_neighbors[:, :, None] * E_neighbors, axis=1)  # shape: (N_p, 3)
+  
             # print('FD version:')
             # print(f"RF:  ({self.forces_rf[0][0]}, {self.forces_rf[0][1]}, {self.forces_rf[0][2]})")
             # print(f"     ({self.forces_rf[1][0]}, {self.forces_rf[1][1]}, {self.forces_rf[1][2]})\n")
@@ -427,7 +443,7 @@ class Particles:
                                     term_fwd = dphi_fwd * der_eps
                                 
                                 contrib = phi_center * (term_bwd + term_fwd)     
-                                self.forces_db[a] -= contrib #* h / (8 * np.pi)
+                                self.forces_db[a] -= contrib 
             
             self.forces_db *= h / (8 * np.pi)
                                   