Fixed reaction field forces with only phi_s. Removed computation phi_vacuum

maze_poisson/cli/runners/main_Maze_md.py
@@ -90,8 +90,9 @@ def main(grid_setting, output_settings, md_variables):
     
     # initialize the electrostatic field with CG                  
     if preconditioning == "Yes":
-        grid.phi_prev, _ = PrecondLinearConjGradPoisson(- 4 * np.pi * grid.q / h, tol=tol)
-        if method == 'PB MaZe':
+        if method == 'Poisson MaZe':
+            grid.phi_prev, _ = PrecondLinearConjGradPoisson(- 4 * np.pi * grid.q / h, tol=tol)
+        elif method == 'PB MaZe':
             grid.phi_s_prev, _ = PrecondLinearConjGradPoisson_PB_Jacobi(- 4 * np.pi * grid.q / h, grid, tol=tol)
 
     if not_elec:
@@ -117,7 +118,7 @@ def main(grid_setting, output_settings, md_variables):
     elif md_variables.integrator == 'VV':
         grid.particles = VerletSolutePart1(grid, thermostat=thermostat)
     elif md_variables.integrator == 'manual':
-        grid.particles.pos[0,:] += md_variables.delta
+        # grid.particles.pos[0,:] += md_variables.delta
         grid.particles.pos[1,:] -= md_variables.delta
         # grid.particles.pos[1,:] += md_variables.delta
 
@@ -134,8 +135,9 @@ def main(grid_setting, output_settings, md_variables):
         grid.UpdateEpsAndK2_transition()
 
     if preconditioning == "Yes":
-        grid.phi, _ = PrecondLinearConjGradPoisson(- 4 * np.pi * grid.q / h, tol=tol, x0=grid.phi_prev)
-        if method == 'PB MaZe':
+        if method == 'Poisson MaZe':
+            grid.phi, _ = PrecondLinearConjGradPoisson(- 4 * np.pi * grid.q / h, tol=tol, x0=grid.phi_prev)
+        elif method == 'PB MaZe':
             grid.phi_s, _ = PrecondLinearConjGradPoisson_PB_Jacobi(- 4 * np.pi * grid.q / h, grid, tol=tol, x0=grid.phi_s_prev)
 
     if md_variables.integrator == 'OVRVO':
@@ -182,8 +184,9 @@ def main(grid_setting, output_settings, md_variables):
     # print('Number of initialization steps:', init_steps,'\n')
     logger.info('Number of initialization steps '+str(init_steps))
 
-    y = np.zeros_like(grid.q) 
-    if method == 'PB MaZe':
+    if method == 'Poisson MaZe':
+        y = np.zeros_like(grid.q) 
+    elif method == 'PB MaZe':
         y_s = np.zeros_like(grid.q)
 
     ######################################### Verlet ############################################
@@ -199,7 +202,7 @@ def main(grid_setting, output_settings, md_variables):
         elif md_variables.integrator == 'VV':
             grid.particles = VerletSolutePart1(grid, thermostat = thermostat)
         elif md_variables.integrator == 'manual':
-            grid.particles.pos[0,:] += md_variables.delta
+            # grid.particles.pos[0,:] += md_variables.delta
             grid.particles.pos[1,:] -= md_variables.delta
             # grid.particles.pos[1,:] += md_variables.delta
 
@@ -211,12 +214,13 @@ def main(grid_setting, output_settings, md_variables):
             # grid.SetCharges(uniform=True)
             grid.SetCharges(uniform=False)
             
-            # apply Verlet algorithm
-            start_Verlet = time.time()
-            grid, y, iter_conv = VerletPoisson(grid, y=y)
-            end_Verlet = time.time()
+            if method == 'Poisson MaZe':
+                # apply Verlet algorithm
+                start_Verlet = time.time()
+                grid, y, iter_conv = VerletPoisson(grid, y=y)
+                end_Verlet = time.time()
 
-            if method == 'PB MaZe':
+            elif method == 'PB MaZe':
                 start_update = time.time()
                 # grid.UpdateEpsAndK2()
                 grid.UpdateEpsAndK2_transition()
@@ -304,15 +308,16 @@ def main(grid_setting, output_settings, md_variables):
                 elif method == 'PB MaZe':
                     ofiles.file_output_performance.write(str(i - init_steps) + ',' + str(end_Verlet - start_Verlet) + ',' + str(iter_conv) + ',' + str(end_VerletPB - start_VerletPB) + ',' + str(iter_conv_PB) + ',' + str(end_update - start_update) + "\n")
             if output_settings.print_field and elec:
-                field_x_MaZe = np.array([grid.phi[l, j, k] for l in range(N)])
                 if method == 'Poisson MaZe':
+                    field_x_MaZe = np.array([grid.phi[l, j, k] for l in range(N)])
+                    
                     for n in range(N):
                         ofiles.file_output_field.write(str(i - init_steps) + ',' + str(X[n] * a0) + ',' + str(field_x_MaZe[n] * V) + '\n')
                 elif method == 'PB MaZe':
                     field_x_MaZe_s = np.array([grid.phi_s[l, j, k] for l in range(N)])
                 
                     for n in range(N):
-                        ofiles.file_output_field.write(str(i - init_steps) + ',' + str(X[n] * a0) + ',' + str(field_x_MaZe[n] * V) + ',' + str(field_x_MaZe_s[n] * V) + '\n')
+                        ofiles.file_output_field.write(str(i - init_steps) + ',' + str(X[n] * a0) + ',' + str(field_x_MaZe_s[n] * V) + ',' + str(field_x_MaZe_s[n] * V) + '\n')
 
     if output_settings.generate_restart_file:
         ofiles.file_output_solute.flush()

maze_poisson/input.py
@@ -100,7 +100,7 @@ class GridSetting:
     @property
     def restart_file(self):
         if self._restart_file is None:
-            self._restart_file = 'restart_files/restart_N'+str(self.N)+'_step9999.csv'
+            self._restart_file = 'restart_files/restart'+str(self.N_p)+'.csv'
         return self._restart_file
 
 ###################################################################################
@@ -122,14 +122,14 @@ class MDVariables:
         self.elec = None # to move
         self.not_elec = None # to move
         self.potential = 'TF' # Tosi Fumi (TF) or Leonard Jones (LJ)
-        self.integrator = 'manual' # OVRVO or VV (Velocity Verlet) or manual (fixed displacement, useful for testing)
+        self.integrator = None # OVRVO or VV (Velocity Verlet) or manual (fixed displacement, useful for testing)
         self.gamma = 1e-3 # OVRVO parameter
         self.method = None
         self.non_polar = False
         self.gamma_np = None
         self.beta_np = None
         self.probe_radius = 1.4
-        self.delta = np.array([0.005, 0., 0.]) / a0
+        self.delta = np.array([0., 0.005, 0.]) / a0 #np.array([0.005, 0., 0.]) / a0
         self.benoit = False # to check Benoit's formula for the reaction field force
 
     @property

maze_poisson/particles.py
@@ -302,12 +302,12 @@ class Particles:
         ''' Compute electrostatic forces (DB and IB from Eq. (30) - (32)) from Im., RF from our approach'''
         h = self.grid.h
         L = self.grid.L
-        phi_v = self.grid.phi_prev if prev else self.grid.phi
+        # phi_v = self.grid.phi_prev if prev else self.grid.phi
         phi_s = self.grid.phi_s_prev if prev else self.grid.phi_s
         w = self.grid.w
    
         #### REACTION FIELD FORCE ####
-        delta_phi = phi_s - phi_v
+        # delta_phi = phi_s - phi_v
 
         if self.grid.md_variables.benoit:
             ### BENOIT'S VERSION ###
@@ -332,13 +332,20 @@ class Particles:
             ix = self.neighbors[:, :, 0]
             iy = self.neighbors[:, :, 1]
             iz = self.neighbors[:, :, 2]
-            delta_phi_values = delta_phi[ix, iy, iz]
-
+            # delta_phi_values = delta_phi[ix, iy, iz]
+            phi_values = phi_s[ix, iy, iz]  # (N_p, 8)
+            
             # Calcola la forza reaction field per ciascuna particella: (N_p, 3)
+            # self.forces_rf = -self.charges[:, None] * np.stack([
+            #     np.sum(delta_phi_values * gpx * gy * gz, axis=1),
+            #     np.sum(delta_phi_values * gx * gpy * gz, axis=1),
+            #     np.sum(delta_phi_values * gx * gy * gpz, axis=1)
+            # ], axis=1)  # shape: (N_p, 3)
+            
             self.forces_rf = -self.charges[:, None] * np.stack([
-                np.sum(delta_phi_values * gpx * gy * gz, axis=1),
-                np.sum(delta_phi_values * gx * gpy * gz, axis=1),
-                np.sum(delta_phi_values * gx * gy * gpz, axis=1)
+                np.sum(phi_values * gpx * gy * gz, axis=1),
+                np.sum(phi_values * gx * gpy * gz, axis=1),
+                np.sum(phi_values * gx * gy * gpz, axis=1)
             ], axis=1)  # shape: (N_p, 3)
             
             # print('\nBenoit version:')
@@ -346,9 +353,9 @@ class Particles:
             # print(f"     ({self.forces_rf[1][0]}, {self.forces_rf[1][1]}, {self.forces_rf[1][2]})\n")
         else:
             ### Electric field version ###
-            E_x = (np.roll(delta_phi, -1, axis=0) - np.roll(delta_phi, 1, axis=0)) / (2 * h)
-            E_y = (np.roll(delta_phi, -1, axis=1) - np.roll(delta_phi, 1, axis=1)) / (2 * h)
-            E_z = (np.roll(delta_phi, -1, axis=2) - np.roll(delta_phi, 1, axis=2)) / (2 * h)
+            E_x = (np.roll(phi_s, -1, axis=0) - np.roll(phi_s, 1, axis=0)) / (2 * h)
+            E_y = (np.roll(phi_s, -1, axis=1) - np.roll(phi_s, 1, axis=1)) / (2 * h)
+            E_z = (np.roll(phi_s, -1, axis=2) - np.roll(phi_s, 1, axis=2)) / (2 * h)
             
             neighbors = self.neighbors  # shape: (n_particles, 8, 3)
             neigh_coords = neighbors * h  # (N_p, 8, 3)