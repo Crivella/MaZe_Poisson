c82e68a7b88d76f256c4fe9f555da18344e30a5b
crivella <davide.grassano@epfl.ch>
2025-07-30 11:53:59 +0200

    Betted dependencies

--- maze_poisson/myio/progress_bar.py
+++ maze_poisson/myio/progress_bar.py
@@ -3,14 +3,27 @@ from . import get_enabled_io
 try:
     from tqdm import tqdm
 except ImportError:
-    def tqdm(x, *args, **kwargs):
-        return x
-
-class ProgressBar:
-    def __init__(self, n: int):
-        self.n = n
-        
-    def __iter__(self):
-        if get_enabled_io():
-            return iter(tqdm(range(self.n)))
-        return iter(range(self.n))
+    class ProgressBar:
+        def __init__(self, n: int):
+            self.n = n
+            self.current = 0
+            
+        def __iter__(self):
+            return self
+        def __next__(self):
+            if self.current >= self.n:
+                raise StopIteration
+            self.current += 1
+            if self.current % 100 == 0:
+                if get_enabled_io():
+                    print(f"Progress: {self.current}/{self.n}")
+            return self.current - 1
+else:
+    class ProgressBar:
+        def __init__(self, n: int):
+            self.n = n
+            
+        def __iter__(self):
+            if get_enabled_io():
+                return iter(tqdm(range(self.n)))
+            return iter(range(self.n))
