8c4d5e473548ceb08a13e5ff225145f845e0b799
crivella <davidecrivella@yahoo.it>
2026-06-04 10:44:51 +0200

    Also test FFT grid on multigrid for combo with GAUSS smoothing

--- library/src/grid_multigrid.c
+++ library/src/grid_multigrid.c
@@ -16,7 +16,9 @@ void multigrid_grid_init(grid * grid) {
     int n = grid->n;
     long int n2 = n * n;
 
-    grid_init_mpi(grid);
+    // TODO: revert this done for testing
+    // grid_init_mpi(grid);
+    grid_init_mpi_fft(grid);
 
     long int size = grid->n_local * n2;
     grid->size = size;
